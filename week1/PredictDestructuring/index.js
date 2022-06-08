dfd06ae59cd86c977430ba2d1b1d940b64d549aa
Herlan Banegas <herlan.banegas21711@gmail.com>
2022-06-08 12:03:04 -0400

    Predictions

---
+++ week1/PredictDestructuring/index.js
@@ -0,0 +1,74 @@
+//Problem 1
+// const cars = ['Tesla', 'Mercedes', 'Honda']
+// const [ randomCar ] = cars
+// const [ ,otherRandomCar ] = cars
+// //Predict the output
+// console.log(randomCar)
+// console.log(otherRandomCar)
+
+//Prediction 1
+//1.Tesla
+//2.Mercedes
+
+//Problem 2 
+// const employee = {
+//     name: 'Elon',
+//     age: 47,
+//     company: 'Tesla'
+// }
+// const { name: otherName } = employee;
+// //Predict the output
+// console.log(name);
+// console.log(otherName);
+
+// Prediction 2
+//error name is not defined
+//otherName Elon
+
+
+//Problem 3
+// const person = {
+//     name: 'Phil Smith',
+//     age: 47,
+//     height: '6 feet'
+// }
+// const password = '12345';
+// const { password: hashedPassword } = person;  
+// //Predict the output
+// console.log(password);
+// console.log(hashedPassword);
+
+// Prediction 3
+//12345
+// password is undefined in person
+
+//Problem 4
+// const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+// const [,first] = numbers;
+// const [,,,second] = numbers;
+// const [,,,,,,,,third] = numbers;
+// //Predict the output
+// console.log(first == second);
+// console.log(first == third);
+
+//Prediction 4
+// first == second is flase 2 != 5
+// first == third is true 2 = 2 
+
+//Problem 5
+// const lastTest = {
+//     key: 'value',
+//     secondKey: [1, 5, 1, 8, 3, 3]
+// }
+// const { key } = lastTest;
+// const { secondKey } = lastTest;
+// const [ ,willThisWork] = secondKey;
+//Predict the output
+// console.log(key);
+// console.log(secondKey);
+// console.log(secondKey[0]);
+// console.log(willThisWork);
+
+//Prediction 5
+//1
+//5
