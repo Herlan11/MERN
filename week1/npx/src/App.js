f357d1e750d46e44dcd4a9835f49c682e17e82a1
Herlan Banegas <herlan.banegas21711@gmail.com>
2022-06-12 15:15:14 -0400

    firstJsx

---
+++ week1/npx/src/App.js
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <><div
+      style={{
+        fontSize: '3rem',
+      }}
+    >Hello Dojo !
+    </div>
+    <div>
+      <h2>Things I need to do:</h2>
+    </div>
+    <div>
+      <uli>
+        <li>Learn React</li>
+        <li>Drive a Boat</li>
+        <li>Fly a Plane</li>
+        <li>Hunt</li>
+      </uli>
+      </div></>
+  );
+}
+
+export default App;
